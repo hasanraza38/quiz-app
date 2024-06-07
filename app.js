c179feff859b313e6d40ce17099841b63d1a5fcc
hasanraza38 <92hasanraza689@gmail.com>
2024-06-07 19:23:17 +0500

    updates

--- app.js
+++ app.js
@@ -3,27 +3,37 @@ const email =document.querySelector('#email')
 const password =document.querySelector('#password')
 const loginBtn =document.querySelector('#login-btn')
 
-let userData = localStorage.setItem('user',JSON.stringify())
 let userArr;
+let userData = JSON.parse(localStorage.getItem('user'))
 
-
+if (userData === null) {
+    userArr=[]
+    
+} else {
+    userArr=userData
+    
+}
+// userArr===null? userArr=[]:userArr=userData
 
 
 loginBtn.addEventListener('click',(e) => {
     e.preventDefault()
     // user.localStorage.setItem(email.value,password.value )
-    let user={
 
-        email : email.value,
-        password : password.value,
+    localStorage.setItem('user',JSON.stringify(userArr))
+    console.log(userArr);
+    // let user={
+
+    //     email : email.value,
+    //     password : password.value,
     
-    }
+    // }
 
-    let userJson=JSON.stringify(user)
-    let stored =localStorage.setItem('user',userJson)
-    console.log(user);
-    console.log(email.value);
-    console.log(password.value);
+    // let userJson=JSON.stringify(user)
+    // let stored =localStorage.setItem('user',userJson)
+    // console.log(user);
+    // console.log(email.value);
+    // console.log(password.value);
 })
 
 
